35a25d5070559cfa9d127fbd87a7e5bc2ab922e9
kamdibus <kam.dibus@gmail.com>
2018-03-26 00:35:34 +0200

    PIK-5 Add a simple test
    Just an example - should be deleted later on

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
-    
+class HelloController {
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
 }
