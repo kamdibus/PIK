a9e7e1c60e27ce81e259867919374677a50e4c05
kezcolaj <rafalkoguciuk33@gmail.com>
2018-04-14 18:19:04 +0200

    Small change

--- src/main/java/hello/devices/dto/DeviceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hello.devices.dto;
-
-public class DeviceNotFoundException extends RuntimeException {
-    public DeviceNotFoundException(String name){
-        super("No device of name " + name + " found", null, false, false);
-    }
-}
