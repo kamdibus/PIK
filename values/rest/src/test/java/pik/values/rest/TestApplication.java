07fa1fd6eedb78c4d7e5cb976ea39417231e7826
pwilcz <P.WIlczynski@stud.elka.pw.edu.pl>
2018-05-09 18:22:47 +0200

    PIK-26 Value module initial version

---
+++ values/rest/src/test/java/pik/values/rest/TestApplication.java
@@ -0,0 +1,13 @@
+package pik.values.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
+
