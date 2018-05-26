a0f8507e230cfab818a3d991f6b5656df18e2abd
pwilcz <P.WIlczynski@stud.elka.pw.edu.pl>
2018-05-26 16:45:23 +0200

    Device and Variable integration test cd

--- app/src/test/java/pik/DeviceIntegrationTest.java
+++ app/src/test/java/pik/DeviceIntegrationTest.java
@@ -1,6 +1,8 @@
 package pik;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +10,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import pik.devices.domain.DeviceFacade;
+import pik.devices.domain.DeviceRepository;
 import pik.devices.domain.dto.DeviceDTO;
+import pik.devices.domain.dto.VariableDTO;
 
 @RunWith(SpringRunner.class)
 @EnableWebMvc
@@ -25,7 +32,15 @@ public class DeviceIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    DeviceRepository deviceRepository;
+
     private ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void clear() {
+
+    }
 
     @Test
     public void whenDeviceIsAddedStatusIsOk() throws Exception {
@@ -43,6 +58,62 @@ public class DeviceIntegrationTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(device.getName()));
 
+    }
+
+    @Test
+    public void whenDeviceIsAddedCanBeFetched() throws Exception {
+
+        //given device as json
+        DeviceDTO device = new DeviceDTO((long) 0, "DeviceTestName1");
+        String json = mapper.writeValueAsString(device);
+
+        //perform post to /device and expect status is ok
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/device")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        DeviceDTO resultDevice = mapper.readValue(result.getResponse().getContentAsString(), DeviceDTO.class);
+
+        //perfom get to /device and expect added device to be on device list
+        mockMvc.perform(MockMvcRequestBuilders.get("/device/" + resultDevice.getId()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(resultDevice.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(device.getName()));
 
     }
+
+    @Test
+    public void whenVariableIsAddedToExistingDeviceStatusIsOk() throws Exception {
+
+        //given device as json
+        DeviceDTO device = new DeviceDTO((long) 0, "DeviceWithSomeName");
+        String json = mapper.writeValueAsString(device);
+
+        //perform post to /device and expect status is ok
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/device")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(device.getName()));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/device")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        DeviceDTO resultDevice = mapper.readValue(result.getResponse().getContentAsString(), DeviceDTO.class);
+
+        ////perform post to /variable and expect status is ok
+
+    }
+
+
+
 }
