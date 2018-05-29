64a21091574e982de2c2afb69894c850dc99540e
Spinaker1 <klitschko456@gmail.com>
2018-05-30 00:20:13 +0200

    change backend url depending on environment

--- src/frontend/src/url.js
+++ src/frontend/src/url.js
@@ -1,3 +1,11 @@
-const url = 'http://wtxgsb.herokuapp.com/';
+const hostname = window && window.location && window.location.hostname;
+
+let url;
+
+if(hostname === 'localhost') {
+  url = 'http://localhost:8080';
+} else if(hostname === 'wtxgs.herokuapp.com') {
+  url = 'http://wtxgsb.herokuapp.com';
+}
 
 export default url;
