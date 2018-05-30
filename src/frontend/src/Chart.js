f0ffbc71e989b2c1e6625da3e9a9f01b23a13319
Spinaker1 <klitschko456@gmail.com>
2018-05-30 16:14:17 +0200

    Show formated timestamp.

--- src/frontend/src/Chart.js
+++ src/frontend/src/Chart.js
@@ -37,6 +37,22 @@ let chartData = {
         }
     };
 
+    
+
+function timeConverter(UNIX_timestamp){
+  const a = new Date(UNIX_timestamp * 1000);
+  const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  const year = a.getFullYear();
+  const month = months[a.getMonth()];
+  const date = a.getDate();
+  const hour = a.getHours();
+  const min = a.getMinutes();
+  const sec = a.getSeconds();
+  const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
+    
 class Chart extends Component {
   constructor(props) {
     super(props);
@@ -54,7 +70,8 @@ class Chart extends Component {
        let j = this.props.values.length-valuesCount;
        for (var i = 0; i < valuesCount ; i++){
             chartData.datasets[0].data[i] = this.props.values[j].value;
-            chartData.labels.push(this.props.values[j].timestamp);
+            const time = timeConverter(this.props.values[j].timestamp);
+            chartData.labels.push(time);
             j++;
        }
 
