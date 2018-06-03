5ae462d11954419b90824eb19df95673f3948fb2
kamdibus <kam.dibus@gmail.com>
2018-06-04 00:02:39 +0200

    Sleep in consumer
    Signed-off-by: kamdibus <kam.dibus@gmail.com>

--- kafka/consumer/src/main/java/pik/Application.java
+++ kafka/consumer/src/main/java/pik/Application.java
@@ -15,6 +15,11 @@ public class Application {
 
         while(true) {
             consumer.consume();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
